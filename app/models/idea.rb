314605e37559b224db385cee5643297c4583f7e2
Kyra Weber <kyra.m.weber@gmail.com>
2015-05-18 17:44:27 -0600

    added js file

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,6 @@
 class Idea < ActiveRecord::Base
+  enum quality: %w(swill plausible genius)
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :quality, presence: true
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 describe Idea do
-  it "has a title" do
-    idea = Idea.create(title:"good idea",body: "really really good", quality:"swill" )
+  context "is valid" do
+    it "has correct attributes" do
+      idea = Idea.create(title:"good idea",body: "really really good", quality:"swill")
+      expect(idea.title).to eq("good idea")
+      expect(idea.body).to eq("really really good")
+      expect(idea.quality).to eq("swill")
+    end
+  end
+
+  context "is invalid" do
+    it "missing any attribute" do
+      idea = Idea.create(title:"", body: "really really good", quality:"swill")
+      expect(idea).not_to be_valid
+    end
+
   end
 end
